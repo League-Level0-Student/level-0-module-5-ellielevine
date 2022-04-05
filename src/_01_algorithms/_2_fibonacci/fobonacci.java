cb1e758b8ffc74ee9032a1018bc022f0f41a02c3
root <root@d6e7e031c322>
2022-04-05 01:24:58 +0000

    snowflake

--- src/_01_algorithms/_2_fibonacci/fobonacci.java
+++ src/_01_algorithms/_2_fibonacci/fobonacci.java
@@ -6,5 +6,12 @@ int number1 = 0;
 System.out.println(number1);
 int number2 = 1;
 System.out.println(number2);
+
+for (int i=0; i<10; i++) {
+	int sum = number1 + number2;
+number1 = number2;
+number2 = sum;
+System.out.println(sum);
+}
 }
 }
